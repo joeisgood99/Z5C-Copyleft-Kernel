7bf3b83b2a9721e4019bf290df3ee621d20a64c1
ben <joeisgood99@gmail.com>
2016-12-16 11:12:14 +1100

    fix build script to ref new ramdisk

--- joe_MM_build_it4_suzuran.sh
+++ joe_MM_build_it4_suzuran.sh
@@ -37,7 +37,7 @@ fi
 ### E5823
 echo "Building boot_E5823.img from ramdisk, zImage & dt.img ..."
 
-../final_files/mkbootimg --cmdline "androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 dwc3_msm.prop_chg_detect=Y coherent_pool=2M dwc3_msm.hvdcp_max_current=1500 enforcing=0" --base 0x00000000 --kernel arch/arm64/boot/Image.gz-dtb --ramdisk ../final_files/305ramdiskE5823.cpio.gz --ramdisk_offset 0x02000000 --pagesize 4096 -o ../final_files/boot_E5823.img --tags_offset 0x01E00000
+../final_files/mkbootimg --cmdline "androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 dwc3_msm.prop_chg_detect=Y coherent_pool=2M dwc3_msm.hvdcp_max_current=1500 enforcing=0" --base 0x00000000 --kernel arch/arm64/boot/Image.gz-dtb --ramdisk ../final_files/5-11Ramdisk-E5823.cpio.gz --ramdisk_offset 0x02000000 --pagesize 4096 -o ../final_files/boot_E5823.img --tags_offset 0x01E00000
 
 if [ -e ../final_files/boot_E5823.img ]
 then
@@ -45,19 +45,19 @@ then
 ### Zip boot.img
 echo "Creating TWRP installable .zip cont boot.img and modules..."
 cd ../final_files/
-mv $FINALFILES/boot_E5823.img $FINALFILES/New-kali-305boot.img
-zip -r Z5C_kali-305-boot_v.zip New-kali-305boot.img META-INF  
+mv $FINALFILES/boot_E5823.img $FINALFILES/New-kali-5-11-boot.img
+zip -r  New-kali-5-11-boot.img META-INF modules 
 rm -f boot.img
 
 ### Version number
 #echo -n "Enter version number: "
 #read version
 
-mv /home/ben/kernel-Source-Code/final_files/Z5C_kali-305-boot_v.zip  /home/ben/kernel-Source-Code/final_files/Kali-Z5C_Joeisgood99_v$version.zip
+#mv /home/ben/kernel-Source-Code/final_files/Z5C_kali-305-boot_v.zip  /home/ben/kernel-Source-Code/#final_files/Kali-Z5C_Joeisgood99_v$version.zip
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
-echo "Z5C_kali-305-boot_v.zip.zip completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo "Z5C_kali-5-11-boot.zip completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
 else
 echo "Compilation failed! Fix the errors!"
 
